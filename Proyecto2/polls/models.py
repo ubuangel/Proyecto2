85cfeb222841316b9ce1a7fdcaeff263aee3dc54
angel <andresbjar97@gmail.com>
2020-11-11 17:19:35 -0500

    agregando el metodo _str_()para arreglar  <Question: Question object (1)> -usando el shell

--- Proyecto2/polls/models.py
+++ Proyecto2/polls/models.py
@@ -17,9 +17,16 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
- ''' forin key le dice al dj que cada opcion esta relacionada a una pregunta   votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+
+'''forin key le dice al dj que cada opcion esta relacionada 
+ a una pregunta   votes = models.IntegerField(default=0)'''
