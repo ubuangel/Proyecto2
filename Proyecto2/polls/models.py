7e9eb06f1a5f1660327507aad77e50c72f1d1f7a
angel <andresbjar97@gmail.com>
2020-11-11 16:00:43 -0500

    comentando  de models.py

--- Proyecto2/polls/models.py
+++ Proyecto2/polls/models.py
@@ -1,6 +1,15 @@
 '''En nuestra aplicación de encuestas, crearemos dos modelos: Pregunta y Elección. 
 Una pregunta tiene una pregunta y una fecha de publicación. Una elección tiene dos campos: el texto de la elección y un recuento de votos. 
-Cada opción está asociada con una pregunta.'''
+Cada opción está asociada con una pregunta.
+
+
+quí, cada modelo está representado por una clase que
+ es subclase de django.db.models.Model. 
+ Cada modelo tiene una serie de variables de clase, cada una de las cuales 
+representa un campo de base de datos en el modelo.
+
+
+'''
 
 from django.db import models
 
