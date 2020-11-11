7ec9c0d2a73d794efa6f37f64ea40d11a9fd9c2e
angel <andresbjar97@gmail.com>
2020-11-11 16:58:40 -0500

    para incluir la app al proyecto agregamos una referencia a su clase de configuracion

--- Proyecto2/polls/models.py
+++ Proyecto2/polls/models.py
@@ -22,4 +22,4 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+ ''' forin key le dice al dj que cada opcion esta relacionada a una pregunta   votes = models.IntegerField(default=0)
